146d18e81ee6f1cf0dc074673a8ec0116927b2a1
Bowson Bike <calendarlightspeed@gmail.com>
2026-04-16 10:09:15 -0500

    persist chrome profile

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "wxt";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from 'node:path';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -7,4 +8,9 @@ export default defineConfig({
   vite: () => ({
     plugins: [tailwindcss()],
   }),
+  webExt: {
+    // On Windows, the path must be absolute
+    chromiumProfile: resolve('.wxt/chrome-data'),
+    keepProfileChanges: true,
+  }
 });
